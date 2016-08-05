1ac6f1ed79dbfe639a97d45f09a1179891765ce5
Hsu <lhsu@Org.Collegeboard.local>
2016-08-05 18:39:54 -0400

    homebutton fix

--- js/start.js
+++ js/start.js
@@ -2,4 +2,12 @@ console.log('javascript linked');
 
 $(document).ready(function() {
   console.log('jQuery is ready!');
+
+  $('.homebutton').click(function(){
+    window.alert("you clicked the home button");
+
+    $('.like').click(function(){
+      color.change(red);
+    })
+  });
 });
