94345235dd6b28318bd2b3a631a2272a615b7a59
Hsu <lhsu@Org.Collegeboard.local>
2016-08-05 22:55:53 -0400

    animations

--- js/start.js
+++ js/start.js
@@ -5,9 +5,35 @@ $(document).ready(function() {
 
   $('.homebutton').click(function(){
     window.alert("you clicked the home button");
+    });
 
-    $('.like').click(function(){
-      color.change(red);
-    })
+  $('.explore').click(function(){
+    window.alert("you clicked the explore button");
   });
+
+  $('.heart').click(function(){
+    window.alert("you clicked the heart button");
+    for(i = 0; i<10; i++){
+      $('.heart').fadeOut();
+      $('.heart').fadeIn();
+    }
+    });
+
+  $('.profile').click(function(){
+    window.alert("you clicked the profile button");
+    });
+
+  $('.like').click(function(){
+    window.alert("you clicked the like button");
+    $('.like').find('.likebutton');
+    $('.likebutton').attr("src", "icons/redheart.png");
+    $('.like').fadeOut();
+  });
+
+  $('.options').click(function(){
+    window.alert("you clicked the options button");
+    $('.options').animate(
+      { width: '500px', height: '100px'},
+      300);
+    });
 });
